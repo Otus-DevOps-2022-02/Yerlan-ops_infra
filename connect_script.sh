2a4186be39db67417e98f16fa4637b4d1bef555e
Yerlan Massalin <yerlanmass.pm@gmail.com>
2022-07-21 12:15:48 +0600

    3 files: mongodb_connect, startupserver.sh and connect_script.sh were amended

--- connect_script.sh
+++ connect_script.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-ipaddress=$(yc compute instance get reddit-app4 --format json | jq -rC '.network_interfaces' | awk '/one_to_one_nat/ {getline; print $2}' | awk -F \" '{print $2}')
-portnumber=$(ssh -i ~/.ssh/appuser yc-user@$ipaddress ps -aux | awk '$NF=="[reddit]"{n=split($0,t,/[):]/);print t[n-1]}')
+ipaddress=$(yc compute instance get reddit-app3 --format json | jq -rC '.network_interfaces' | awk '/one_to_one_nat/ {getline; print $2}' | awk -F \" '{print $2}')
+for items in {1..10};
+
+do
+		sleep 13
+		portnumber=$(ssh -i ~/.ssh/appuser yc-user@$ipaddress ps -aux | awk '$NF=="[reddit]"{n=split($0,t,/[):]/);print t[n-1]}')
+			if [[ -n "$portnumber" ]]
+			then
+				break;
+			fi
+done & PID=$!
+i=1
+sp="/-\|"
+echo -n ' '
+while [ -d /proc/$PID ]
+do
+  printf "\b${sp:i++%${#sp}:1}"
+done
 
 args="$ipaddress:$portnumber"
 curl -f -s --connect-timeout 7 -s $args > /dev/null && \
--- install_mongodb.sh
+++ install_mongodb.sh
@@ -5,7 +5,7 @@ echo "######   add key and repo for mongodb   ######"
 echo "##############################################"
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo " [ arch=amd64 ] deb https://mirror.yandex.ru/mirrors/repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+echo " deb [ arch=amd64 ] https://mirror.yandex.ru/mirrors/repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 
 echo "##############################################"
 echo "######       INSTALLING mongodb...      ######"
