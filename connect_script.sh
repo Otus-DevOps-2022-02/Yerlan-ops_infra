56d136fea0718b9d49cb0573b2a6540c78926c6f
Yerlan Massalin <yerlanmass.pm@gmail.com>
2022-08-05 17:36:41 +0600

    new script file was added

--- connect_script.sh
+++ connect_script.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 ipaddress=$(yc compute instance get reddit-app3 --format json | jq -rC '.network_interfaces' | awk '/one_to_one_nat/ {getline; print $2}' | awk -F \" '{print $2}')
-for items in {1..10};
+echo $ipaddress
+portnumber=0
+c=0
+
+while [[ $c -lt 10 ]]
 
 do
-		sleep 13
-		portnumber=$(ssh -i ~/.ssh/appuser yc-user@$ipaddress ps -aux | awk '$NF=="[reddit]"{n=split($0,t,/[):]/);print t[n-1]}')
-			if [[ -n "$portnumber" ]]
-			then
-				break;
-			fi
+  echo "Number: $c"
+  ((c++))
+  wait
+  if [[ $portnumber -gt 0 ]]; then
+
+    break
+
+  fi
+  portnumber=$(ssh -o StrictHostKeyChecking=no -i ~/.ssh/appuser yc-user@$ipaddress ps -aux | awk '$NF=="[reddit]"{n=split($0,t,/[):]/);print t[n-1]}')
+  wait
+  echo $portnumber
 done & PID=$!
 i=1
 sp="/-\|"
@@ -17,9 +26,14 @@ echo -n ' '
 while [ -d /proc/$PID ]
 do
   printf "\b${sp:i++%${#sp}:1}"
+  sleep 0.3
 done
 
 args="$ipaddress:$portnumber"
+echo $args
+echo "ipaddress is: " $ipaddress
+echo "port is: " $portnumber
+
 curl -f -s --connect-timeout 7 -s $args > /dev/null && \
-echo "#### Reddit app connection successfully tested! ####" || \
+echo "#### Puma app connection successfully tested! ####" || \
 echo "######     Connection timed out..     ########"
---
+++ run_scripts.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sh ./install_ruby.sh && sh ./install_mongodb.sh && sh ./deploy.sh
